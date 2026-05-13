145b1984d72324ae740a1c6a08dafdf2e4233206
zerox374 <kapisettisowmya@gmail.com>
2026-05-13 09:33:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   token: process.env.DISCORD_TOKEN,
   prefix: "!",
   enablePrefix: true,
-  supportServer: "https://discord.gg/9MVAPpfs8D",
+  supportServer: "https://discord.gg/qyBSqpWgr",
 
   activity: {
     name: "/help",
